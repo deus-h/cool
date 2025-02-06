2dd988e58e9c2cb8a4931e22a3ae905d48e4ca61
deus-h <amadeus.hritani@simhop.se>
2025-02-06 14:21:17 +0100

    Added SimHop AI

---
+++ src/components/SimHopAI.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { BubbleChat } from 'flowise-embed-react'
+
+const SimHopAI = () => {
+    return (
+        <BubbleChat
+            chatflowid="3716fd72-74df-4281-ba3f-15ef0af6b27f"
+            apiHost="https://ai.simhop.se"
+        />
+    );
+};
+
+export default SimHopAI;
